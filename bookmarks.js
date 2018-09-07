86f3e020b867e05f7a535e45c97611a9bcdf3c1b
ScanuNicco <Scanuproductions@gmail.com>
2018-09-06 17:36:52 -0700

    More updates

--- bookmarks.js
+++ bookmarks.js
@@ -73,4 +73,22 @@ function deleteBookmark(){
 	localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
 }
 
+var BrowserWindow = require('electron').remote;
+var theWindow = BrowserWindow.getCurrentWindow();
+
+function closeWindow(){
+	theWindow.close();
+}
+
+function maxWindow() {
+	if (!theWindow.isMaximized()) {
+        theWindow.maximize();          
+    } else {
+        theWindow.unmaximize();
+    }
+}
+
+function minWindow() {
+	theWindow.minimize(); 
+}
 scanEntries();
--- settings.js
+++ settings.js
@@ -56,3 +56,22 @@ function setNT() {
 	localStorage.setItem("nttype", NTtype);
     document.getElementById("restartRequired").style.display = "block";
 }
+
+var BrowserWindow = require('electron').remote;
+var theWindow = BrowserWindow.getCurrentWindow();
+
+function closeWindow(){
+	theWindow.close();
+}
+
+function maxWindow() {
+	if (!theWindow.isMaximized()) {
+        theWindow.maximize();          
+    } else {
+        theWindow.unmaximize();
+    }
+}
+
+function minWindow() {
+	theWindow.minimize(); 
+}
